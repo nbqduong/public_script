310f6b259012dd03cba3675ee851a39151974e47
nbqduong <nbqduong@gmail.com>
2026-09-22 10:46:50 +0700

    update report foler

--- docker-test.sh
+++ docker-test.sh
@@ -6,10 +6,14 @@ sudo ufw enable
 
 docker pull ghcr.io/nbqduong/simbolt-playwrightloadtest:latest
 
+mkdir result
+chmod +777 result
+
 docker run -d \
   --name simbolt \
   --restart unless-stopped \
   -p 8080:8080 \
   --env-file .env \
+  -v ./result:/app/test-results \
   ghcr.io/nbqduong/simbolt-playwrightloadtest:latest
 
